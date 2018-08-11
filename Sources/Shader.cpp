cf92e133a664fe2da039a742a88c099503b9861c
Polona Caserman <polona.caserman@gmail.com>
2018-08-11 18:42:03 +0200

    Render tree

--- Sources/Shader.cpp
+++ Sources/Shader.cpp
@@ -9,7 +9,7 @@
 #include <Kore/Input/Keyboard.h>
 #include <Kore/Log.h>
 
-#include "MeshObject.h"
+#include "Baum.h"
 #include "water.h"
 
 using namespace Kore;
@@ -24,31 +24,8 @@ namespace {
 	const float CAMERA_ROTATION_SPEED = 0.05f;
 	const float CAMERA_MOVE_SPEED = 4.f;
 	
-	Graphics4::Shader* vertexShader;
-	Graphics4::Shader* fragmentShader;
-	Graphics4::PipelineState* pipeline;
-	Graphics4::VertexBuffer* vertices;
-	Graphics4::IndexBuffer* indices;
-	
-	// Tree shader
 	bool renderTrees = true;
-	Graphics4::VertexStructure structure_tree;
-	Graphics4::Shader* vertexShader_tree;
-	Graphics4::Shader* fragmentShader_tree;
-	Graphics4::PipelineState* pipeline_tree;
-	
-	Graphics4::TextureUnit tex_tree;
-	Graphics4::ConstantLocation pLocation_tree;
-	Graphics4::ConstantLocation vLocation_tree;
-	Graphics4::ConstantLocation mLocation_tree;
-	Graphics4::ConstantLocation mLocation_tree_inverse;
-	Graphics4::ConstantLocation diffuse_tree;
-	Graphics4::ConstantLocation specular_tree;
-	Graphics4::ConstantLocation specular_power_tree;
-	Graphics4::ConstantLocation lightPosLocation_tree;
-	Graphics4::ConstantLocation lightCount_tree;
-	
-	MeshObject* tree;
+	Baum* tree;
 	
 	// Keyboard controls
 	bool rotate = false;
@@ -108,20 +85,14 @@ namespace {
 		Graphics4::begin();
 		Graphics4::clear(Graphics4::ClearColorFlag | Graphics4::ClearDepthFlag, Kore::Graphics1::Color::Green, 1.0f, 0);
 		
-		if (renderTrees) {
-			mat4 P = getProjectionMatrix();
-			mat4 V = getViewMatrix();
-		
-			Graphics4::setPipeline(pipeline_tree);
-			
-			Graphics4::setMatrix(vLocation_tree, V);
-			Graphics4::setMatrix(pLocation_tree, P);
-			
-			tree->setLights(lightCount_tree, lightPosLocation_tree);
-			tree->render(tex_tree, mLocation_tree, mLocation_tree_inverse, diffuse_tree, specular_tree, specular_power_tree);
-		}
+		mat4 P = getProjectionMatrix();
+		mat4 V = getViewMatrix();
 
-		renderWater(getProjectionMatrix() * getViewMatrix(), 0.0f);
+		renderWater(P * V, 0.0f);
+		
+		if (renderTrees) {
+			tree->render(P, V);
+		}
 
 		Graphics4::end();
 		Graphics4::swapBuffers();
@@ -202,43 +173,6 @@ void keyUp(KeyCode code) {
 	}
 }
 
-void loadTreeShader() {
-	FileReader vs("shader_tree.vert");
-	FileReader fs("shader_tree.frag");
-	vertexShader_tree = new Graphics4::Shader(vs.readAll(), vs.size(), Graphics4::VertexShader);
-	fragmentShader_tree = new Graphics4::Shader(fs.readAll(), fs.size(), Graphics4::FragmentShader);
-	
-	structure_tree.add("pos", Graphics4::Float3VertexData);
-	structure_tree.add("tex", Graphics4::Float2VertexData);
-	structure_tree.add("nor", Graphics4::Float3VertexData);
-	
-	pipeline_tree = new Graphics4::PipelineState();
-	pipeline_tree->inputLayout[0] = &structure_tree;
-	pipeline_tree->inputLayout[1] = nullptr;
-	pipeline_tree->vertexShader = vertexShader_tree;
-	pipeline_tree->fragmentShader = fragmentShader_tree;
-	pipeline_tree->depthMode = Graphics4::ZCompareLess;
-	pipeline_tree->depthWrite = true;
-	pipeline_tree->blendSource = Graphics4::SourceAlpha;
-	pipeline_tree->blendDestination = Graphics4::InverseSourceAlpha;
-	pipeline_tree->alphaBlendSource = Graphics4::SourceAlpha;
-	pipeline_tree->alphaBlendDestination = Graphics4::InverseSourceAlpha;
-	pipeline_tree->compile();
-	
-	tex_tree = pipeline_tree->getTextureUnit("tex");
-	Graphics4::setTextureAddressing(tex_tree, Graphics4::U, Graphics4::Repeat);
-	Graphics4::setTextureAddressing(tex_tree, Graphics4::V, Graphics4::Repeat);
-	
-	pLocation_tree = pipeline_tree->getConstantLocation("P");
-	vLocation_tree = pipeline_tree->getConstantLocation("V");
-	mLocation_tree = pipeline_tree->getConstantLocation("M");
-	mLocation_tree_inverse = pipeline_tree->getConstantLocation("MInverse");
-	diffuse_tree = pipeline_tree->getConstantLocation("diffuseCol");
-	specular_tree = pipeline_tree->getConstantLocation("specularCol");
-	specular_power_tree = pipeline_tree->getConstantLocation("specularPow");
-	lightPosLocation_tree = pipeline_tree->getConstantLocation("lightPos");
-	lightCount_tree = pipeline_tree->getConstantLocation("numLights");
-}
 
 int kore(int argc, char** argv) {
 	Kore::System::init("Shader", width, height);
@@ -249,12 +183,7 @@ int kore(int argc, char** argv) {
 	Keyboard::the()->KeyDown = keyDown;
 	Keyboard::the()->KeyUp = keyUp;
 
-	loadTreeShader();
-	tree = new MeshObject("Tree02/tree02.ogex", "Tree02/", structure_tree, 1.0);
-	Kore::Quaternion treeRot = Kore::Quaternion(0, 0, 0, 1);
-	treeRot.rotate(Kore::Quaternion(vec3(1, 0, 0), -Kore::pi / 2.0));
-	treeRot.rotate(Kore::Quaternion(vec3(0, 0, 1), Kore::pi / 2.0));
-	tree->M = mat4::Translation(0, 0, 0) * treeRot.matrix().Transpose();
+	tree = new Baum("Tree02/tree02.ogex", "Tree02/");
 	
 	cameraPos = vec3(-5, 5, 5);
 
